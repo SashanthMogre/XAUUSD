0cfe168b8eb8dd9c6db3e89fb34be6e7f9f738f1
Sashanth Mogre <sashanthmogrr@gmail.com>
2025-05-16 21:49:24 +0530

    setting up config and artifacts

--- XAUUSD/exception/exception.py
+++ XAUUSD/exception/exception.py
@@ -1,7 +1,7 @@
 import sys
 from XAUUSD.logging import logger
 
-class XAUUSD(Exception):
+class XAUUSDException(Exception):
     def __init__(self,error_message,error_details:sys):
         self.error_message = error_message
         _,_,exc_tb = error_details.exc_info()
