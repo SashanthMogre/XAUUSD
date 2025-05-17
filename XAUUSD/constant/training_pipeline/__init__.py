93a9e95427ed9e957db0f7d723315d5c681de5b3
Sashanth Mogre <sashanthmogrr@gmail.com>
2025-05-17 23:40:28 +0530

    data ingesetion start

---
+++ XAUUSD/constant/training_pipeline/__init__.py
@@ -0,0 +1,18 @@
+import os
+import numpy as np
+
+# Project Identity 
+PIPELINE_NAME: str = "XAUUSD"
+ARTIFACT_DIR: str = "Artifacts"
+FILE_NAME: str = "xauusd.xlsx"  
+
+# Output CSV Names
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+# Data Ingestion Config
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_SPLIT_DATE: str = "2025-03-01"
+
